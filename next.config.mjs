0ea59ea929d971ca6a4d54f9cdb15d5374d9637f
unknown <himanshujangir16@gmail.com>
2024-10-29 14:34:36 +0530

    latest commit

--- next.config.mjs
+++ next.config.mjs
@@ -9,18 +9,6 @@ const nextConfig = {
       },
     ],
   },
-  experimental: {
-    serverActions: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push({
-        'utf-8-validate': 'commonjs utf-8-validate',
-        'bufferutil': 'commonjs bufferutil',
-      })
-    }
-    return config
-  },
 };
 
 export default nextConfig;
